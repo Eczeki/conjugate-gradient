57408cc2ac25afd86b7b1d04e0d9ebb43f70d5d4
paweln66 <paweln66@2ae7a310-b2ee-c2d3-cc6f-0c5b87d0802f>
2012-03-03 14:01:36 +0000

    Added conjugate gradient method algorithm

--- ConjugateMatrix/main.cpp
+++ ConjugateMatrix/main.cpp
@@ -40,30 +40,10 @@ void deleteMatrix(double ** matrix	, std::size_t size );										//Function, wh
 bool isSymetric  (double ** matrix	, std::size_t size );										//Function, which check the matrix.
 bool isPositiveDefinite(double ** matrix , std::size_t size,double x);							//Function, which check whether matrix is positive definte
 double ** transpose(unsigned const rows , unsigned const columns ,  double const ** matrix);	//Function, which transpose matrix
-
-
-bool isPositiveDefinite(double ** matrix , std::size_t size , double x)
-{
-	/*	x^T * A * x>0 */
-	return true;
-}
-
-
-
-
-void ConjugateGradientMethod(const char * from , const char * to)
-{
-	std::size_t size = 0;																//matrix[size][size]
-	size << atoi(from);																//the first element in file is size of matrix
-	
-	if(size<=0){	error("Wrong size of matrix in file...:",from);	}
-
-	double ** matrix=0;
-	createMatrix(matrix, size, size);													//Create matrix A
-
-
-	deleteMatrix(matrix,size);														//delete matrix A
-}
+double multiply_Ax_rows_vector(double ** A, unsigned rA, double ** x, unsigned  rx, unsigned cx, unsigned size);
+void ConjugateGradientMethod(const char * from , const char * to);
+double multiplyVectors(double ** v1, unsigned i1, double ** v2, unsigned i2, unsigned size);
+double multiply_pT_A_p(double ** p, double ** A, unsigned k, unsigned size);
 
 int main(int argc , char * argv[])
 {
@@ -82,6 +62,90 @@ int main(int argc , char * argv[])
 	from.close();
 	to.close();
 }
+
+bool isPositiveDefinite(double ** matrix , std::size_t size , double x)
+{
+	/*	x^T * A * x>0 */
+	return true;
+}
+
+void ConjugateGradientMethod(const char * from , const char * to)
+{
+	std::size_t size = 0;																//matrix[size][size]
+	size << atoi(from);																//the first element in file is size of matrix
+	
+	if(size<=0){	error("Wrong size of matrix in file...:",from);	}
+
+	double ** A = 0;
+	const std::size_t max_iter = 100000;		//max iteration
+	const int min_R = 0.0001;
+	double ** r = 0;	//mo¿na tego te¿ nie robiæ na macierzy i nie pamiêtaæ wszystkich zmiennnych, tylko wykorzystaæ tablicê dwuelementow¹
+	double ** p = 0;	//j.w.
+	double ** x = 0;	//j.w.
+	createMatrix(A, size, size);													//Create matrix A
+	createMatrix(r, size, max_iter);		//rows are a vector, columns are a number of element	
+	createMatrix(p, size, max_iter);		//rows are a vector, columns are a number of element
+	createMatrix(x, size, max_iter);		//rows are a vector, columns are a number of element
+	double * alpha = new double[size];
+	double * beta = new double[size];
+
+	double * b =  new double [size];
+
+	//r0 = b - Ax0
+	for(unsigned i = 0; i < size; i++){
+		r[i][0] = b[i] - multiply_Ax_rows_vector(A, i, x, i, 0, size);
+	}
+
+	//p0 = r0
+	for(unsigned i = 0; i < size; i++)
+		r[i][0] = p[i][0];
+
+	//algorithm
+	for(unsigned k = 0; k < max_iter; k++){
+		alpha[k] = multiplyVectors(r, k, r, k, size) / multiply_pT_A_p(p, A, k, size);
+		for(unsigned i = 0; i < size; i++){
+			x[i][k+1] = x[i][k] + alpha[k] * p[i][k];
+			r[i][k+1] = r[i][k] - alpha[k] * multiply_Ax_rows_vector(A, i, p, i, k, size);
+		}
+
+		for(unsigned i = 0; i < size; i++){
+			if( r[i][k+1] < min_R) break;
+		}
+
+		beta[k] = multiplyVectors(r, k, r, k, size);
+		for(unsigned i = 0; i < size; i++){
+			p[i][k+1] = r[i][k+1] + beta[k] * p[i][k];
+		}
+	}
+	//the result is vector x
+ 
+	deleteMatrix(A, size);														//delete matrix A
+}
+
+double multiply_pT_A_p(double ** p, double ** A, unsigned k, unsigned size){
+	double  tmp; 
+	for(unsigned i=0; i < size; i++){
+		tmp += p[i][k] * multiply_Ax_rows_vector(A, i, p, i, k, size);
+	}
+	return tmp;
+}
+
+double multiplyVectors(double ** v1, unsigned i1, double ** v2, unsigned i2, unsigned size){	//multiply two vectors
+	double tmp = 0;
+	for(unsigned i = 0; i<size; i++){
+		tmp += v1[i][i1] * v2[i][i2];
+	}
+	return tmp;
+}
+
+double multiply_Ax_rows_vector(double ** A, unsigned rA, double ** x, unsigned  rx, unsigned cx, unsigned size){
+	double tmp = 0;
+	for(unsigned i = 0; i < size; i++){
+		tmp += A[rA][i] * x[i][cx];
+	}
+	return tmp;
+}
+
 
 void error(const char * p1 , const char * p2/* = ""*/)
 {
@@ -119,7 +183,7 @@ bool isSymetric(double ** matrix , std::size_t size)
 
 double ** transpose(unsigned const rows , unsigned const columns ,  double const ** matrix)
 {
-	double ** _transpose;
+	double ** _transpose  = 0;
 	createMatrix(_transpose,columns,rows);											//This function takes first:matrix,rows,columns but we must 
 																					//swap rows and columns becouse we must allocate memory for 
 																					//new matrix which is transpose.
